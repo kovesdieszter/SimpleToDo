4ce1f2e199f915f052c6a946648d60eb02eeb001
kovesdieszter <kovesdieszter@github.com>
2022-09-28 17:18:08 +0200

    feat: get values from the fields

--- simpletodo/src/components/Container.tsx
+++ simpletodo/src/components/Container.tsx
@@ -1,25 +1,53 @@
 import {UrgentType} from "../enums/UrgentType";
-import {Button, FormControl, InputLabel, MenuItem, Select, TextField}
-    from "@mui/material";
+import {Button, FormControl, InputLabel, MenuItem, Select, TextField} from "@mui/material";
+import React, {useState} from "react";
+import {Task} from "../classes/Task";
 
 
 export const MainContainer = () => {
+
+    const [title, setTitle] = useState("");
+    const [details, setDetails] = useState("");
+    const [urgentType, setUrgentType] = useState(UrgentType.TODAY);
+
+    const tasks: Task[] = [];
+
+    function handleClick(): void {
+        tasks.push(new Task(title, details, urgentType));
+        console.log(tasks);
+        setTitle("");
+        setDetails("");
+        setUrgentType(UrgentType.TODAY);
+    }
+
+
     return (
         <div className={"mainCard"}>
             <div className={"form"}>
-                <div className={"simple"}>
-                    <TextField id="standard-basic" label="Title" variant="standard"/> <p></p>
+                <div className={"simple"} id={"titleText"}>
+                    <TextField id="standard-basic"
+                               label="Title"
+                               variant="standard"
+                               value={title}
+                               onChange={(event) => {setTitle(event.target.value)}}
+                    />
                 </div>
                 <div className={"simple"}>
-                    <TextField className={"simple details"} id="standard-basic" label="Details" variant="standard"/>
-                    <p></p>
+                    <TextField className={"details"}
+                               id="standard-basic detailsText" label="Details"
+                               variant="standard"
+                               value={details}
+                               onChange={(event) => {setDetails(event.target.value)}}
+                    />
                 </div>
                 <div className={"simple"}>
                     <FormControl style={{"width": "200px"}}>
                         <InputLabel id="selectOption">Urgency</InputLabel>
-                        <Select labelId="selectOption" label="Label">
-                            {Object.keys(UrgentType).map((element, index) => (
-                                <MenuItem className={"menuItem"} centerRipple key={index} value={element}>
+                        <Select labelId="selectOption" label="Label" value= {urgentType} onChange={(event) => {
+                            // @ts-ignore
+                            setUrgentType(event.target.value)}}>
+                            {Object.keys(UrgentType).map((element:string, index) => (
+                                <MenuItem className={"menuItem"} id={"menuItem"} centerRipple key={index} value={element} >
                                     {UrgentType[element]}
                                 </MenuItem>
                             ))}
@@ -27,7 +55,9 @@ export const MainContainer = () => {
                     </FormControl>
                 </div>
                 <div className={"simple"}>
-                    <Button className={"addButton"} variant="contained">Add new task</Button>
+                    <Button className={"addButton"} variant="contained" onClick={handleClick}>
+                        Add new task
+                    </Button>
                 </div>
             </div>
         </div>
