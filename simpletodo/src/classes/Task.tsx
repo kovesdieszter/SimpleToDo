205cb0025c8586cebd69b5ac2a2753d439bd2090
kovesdieszter <kovesdieszter@github.com>
2022-09-30 00:21:09 +0200

    feat: add id field to Task

--- simpletodo/src/classes/Task.tsx
+++ simpletodo/src/classes/Task.tsx
@@ -2,6 +2,7 @@ import {UrgentType} from "../enums/UrgentType";
 
 export class Task {
     constructor(
+        readonly id: string,
         readonly title: string,
         readonly details: string,
         readonly urgentType: UrgentType
