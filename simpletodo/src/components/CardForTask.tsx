778700c676c36d07f553d6db1ab0c3aa77b9822d
kovesdieszter <kovesdieszter@github.com>
2022-10-12 10:35:32 +0200

    fix: use object instead of switch case

--- simpletodo/src/components/CardForTask.tsx
+++ simpletodo/src/components/CardForTask.tsx
@@ -1,37 +1,27 @@
 import {Task} from "../classes/Task";
 import '../Card.css';
-import {UrgentType} from "../enums/UrgentType";
 import {Button} from "@mui/material";
+
 
 type props = {
     item: Task,
     finishTask(fnTask: string):void};
 
+
 const CardForTask = ({item, finishTask}: props) => {
-
-    const color = () => {
-        let bcColor = "";
-        switch (item.urgentType) {
-            case UrgentType.TODAY:
-                bcColor = 'rgb(255, 0, 0, .3)';
-                break;
-            case UrgentType.WEEK:
-                bcColor = 'rgb(249, 105, 14, .3)';
-                break;
-            case UrgentType.MONTH:
-                bcColor = 'rgb(255, 255, 0, .3)';
-                break;
-            default:
-                bcColor = 'rgba(51, 170, 51, .3)';
-                break;
-        }
-        return bcColor;
+    const backgroundColors = {
+        TODAY: 'rgb(255, 0, 0, .3)',
+        WEEK: 'rgb(249, 105, 14, .3)',
+        MONTH: 'rgb(255, 255, 0, .3)',
+        YEAR: 'rgba(51, 170, 51, .3)'
     }
-    console.log(color);
 
+    function setBackgroundColor() {
+        return backgroundColors[item.urgentType];
+    }
 
     return (
-        <div className="card" style={{"backgroundColor": color()}}>
+        <div className="card" style={{"backgroundColor": setBackgroundColor()}}>
             <h3 className={"cardTitle"}>{item.title}</h3>
             <p className={"cardDetails"}>{item.details}</p>
             <div className={"buttonDiv"}>
